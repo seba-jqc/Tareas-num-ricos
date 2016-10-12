6bd64a2ef7ed9cf271ed609558f5827773647aef
seba-jqc <sebastian.jorcq@gmail.com>
2016-10-12 14:19:32 -0300

    explicacion de funciones agregado

--- tarea5p2.py
+++ tarea5p2.py
@@ -22,19 +22,34 @@ def f_modelo2(params, x):
     return a1*x+a2-lorentz
 
 def arg_chi_2(params, datos_x, datos_y, func):
+    '''
+    crea el argumento de la funcion X². Este corresponde al array datos_y menos
+    la funcion func.
+    params corresponde a los parametros iniciales sobre los cuales se minimizara
+    la funcion
+    '''
     return datos_y-func(params, datos_x)
 
 def estimador_amp(x):
+    '''
+    Estimador de la amplitud de un conjunto x de datos
+    '''
     x_min=np.amin(x)
     x_max=np.amax(x)
     Amp=x_max-x_min
     return Amp
 
 def estimador_mu(x):
+    '''
+    estimador de mu de un conjunto x de datos.
+    '''
     mu=np.mean(x)
     return mu
 
 def estimador_sigma1(x):
+    '''
+    estimador de sigma (desviacion estandar) de un conjunto x de datos
+    '''
     mu=estimador_mu(x)
     n=len(x)
     suma=0
@@ -49,12 +64,19 @@ def estimador_sigma2(x):
     return sigma
 
 def estimador_pendiente(x,y):
+    '''
+    estimador de la pendiente de la recta de la forma y=f(x). Se usa la
+    convencion m=(y2-y1)/(x2-x1)
+    '''
     l=len(x)
     num=y[l-1]-y[0]
     den=x[l-1]-x[0]
     return num/den
 
 def estimador_cruce(y):
+    '''
+    estimador del valor de y en que la funcion f(x) intersecta al eje y
+    '''
     return y[0]
 
 amp=estimador_amp(flujo)
@@ -74,23 +96,29 @@ resultado_lorentz2=leastsq(arg_chi_2, p1, args=(long_onda, flujo, f_modelo2))
 print resultado_Gauss1[0]
 print resultado_lorentz1[0]
 
-plt.plot(long_onda, f_modelo1(resultado_Gauss1[0], long_onda), '-r', label='fit de Gauss')
+#plt.plot(long_onda, f_modelo1(resultado_Gauss1[0], long_onda), '-r', label='fit de Gauss')
 #plt.plot(long_onda, f_modelo1(resultado_Gauss2[0], long_onda), '-g')
 #plt.plot(long_onda, flujo, 'o')
 #plt.xlabel("Longitud de onda [A]")
 #plt.ylabel("Flujo por unidad de frecuencia [$erg*(cm^{-2}*s^{-1}*Hz^{-1})]$")
 #plt.savefig('Fit_gauss')
 
-plt.plot(long_onda, f_modelo2(resultado_lorentz1[0], long_onda), '-g', label='fit de Lorentz')
+#plt.plot(long_onda, f_modelo2(resultado_lorentz1[0], long_onda), '-g', label='fit de Lorentz')
 #plt.plot(long_onda, f_modelo2(resultado_lorentz2[0], long_onda), '-g')
-plt.plot(long_onda, flujo, 'o')
-plt.xlabel("Longitud de onda [A]")
-plt.ylabel("Flujo por unidad de frecuencia [$erg*(cm^{-2}*s^{-1}*Hz^{-1})]$")
-plt.legend(loc='lower right')
-plt.savefig('Lorentz_vs_gauss')
+#plt.plot(long_onda, flujo, 'o')
+#plt.xlabel("Longitud de onda [A]")
+#plt.ylabel("Flujo por unidad de frecuencia [$erg*(cm^{-2}*s^{-1}*Hz^{-1})]$")
+#plt.legend(loc='lower right')
+#plt.savefig('Lorentz_vs_gauss')
 #plt.show()
 
 def chi2(params, datos_x, datos_y, func):
+    '''
+    se calcula el valor de la funcion X², definida como la suma de los cuadrado
+    del argumento.
+    La funcion arg_chi_2 crea una array con los diferentes valores numericos del
+    argumento, por lo que se usa el producto punto para calcula X²
+    '''
     arg=arg_chi_2(params,datos_x,datos_y,func)
     return np.dot(arg,arg)
 
